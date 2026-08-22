89277c4bfea4184366ffab5f1d5ebd858f55b576
keaofronema1-jpg <keaofronema1@gmail.com>
2026-08-23 00:11:37 +0300

    Update HumanoidMod.java

--- src/main/java/com/humanoid/horror/HumanoidMod.java
+++ src/main/java/com/humanoid/horror/HumanoidMod.java
@@ -1,13 +1,9 @@
 package com.humanoid.horror;
 
-import com.humanoid.horror.entity.Creature3;
+import com.humanoid.horror.event.EntityAttributeRegister;
 import com.humanoid.horror.network.ModMessages;
 import com.humanoid.horror.entity.ModEntities;
 
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -28,38 +24,16 @@ public class HumanoidMod {
         // Entity kayıtları
         ModEntities.ENTITIES.register(modEventBus);
 
-        // Event listener'ları manuel ekliyoruz
+        // Event yönlendirmeleri
         modEventBus.addListener(this::setup);
-        modEventBus.addListener(this::onAttributeCreate);
+        
+        // Attribute kaydı geldiğinde direkt EntityAttributeRegister sınıfına yönlendiriyoruz
+        modEventBus.addListener(EntityAttributeRegister::onAttributeCreate);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             ModMessages.register();
         });
-    }
-
-    private void onAttributeCreate(EntityAttributeCreationEvent event) {
-
-        // HUMANOID
-        if (ModEntities.HUMANOID.isPresent()) {
-            event.put(
-                    ModEntities.HUMANOID.get(),
-                    Monster.createMonsterAttributes()
-                            .add(Attributes.MAX_HEALTH, 40.0D)
-                            .add(Attributes.MOVEMENT_SPEED, 0.3D)
-                            .add(Attributes.ATTACK_DAMAGE, 6.0D)
-                            .add(Attributes.FOLLOW_RANGE, 32.0D)
-                            .build()
-            );
-        }
-
-        // CREATURE3
-        if (ModEntities.CREATURE3.isPresent()) {
-            event.put(
-                    ModEntities.CREATURE3.get(),
-                    Creature3.createAttributes().build()
-            );
-        }
     }
 }
