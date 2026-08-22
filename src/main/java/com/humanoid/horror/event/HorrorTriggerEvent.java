50392bb5ab48b7ad9a9bbb71234e630bf5f663ed
keaofronema1-jpg <keaofronema1@gmail.com>
2026-08-22 17:42:34 +0300

    Create HorrorTriggerEvent.java

---
+++ src/main/java/com/humanoid/horror/event/HorrorTriggerEvent.java
@@ -0,0 +1,10 @@
+package com.humanoid.horror.event;
+
+import net.minecraftforge.eventbus.api.Event;
+
+public class HorrorTriggerEvent extends Event {
+
+    public HorrorTriggerEvent() {
+        super();
+    }
+}
