8ca5e77e78e902a53a9d39d596b50c3f9d03489e
keaofronema1-jpg <keaofronema1@gmail.com>
2026-08-23 00:12:09 +0300

    Update EntityAttributeRegister.java

--- src/main/java/com/humanoid/horror/event/EntityAttributeRegister.java
+++ src/main/java/com/humanoid/horror/event/EntityAttributeRegister.java
@@ -5,19 +5,23 @@ import com.humanoid.horror.entity.Creature2;
 import com.humanoid.horror.entity.Creature3;
 import com.humanoid.horror.entity.Humanoid;
 import com.humanoid.horror.entity.ModEntities;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "humanoid", bus = Mod.EventBusSubscriber.Bus.MOD)
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+
 public class EntityAttributeRegister {
 
-    @SubscribeEvent
     public static void onAttributeCreate(EntityAttributeCreationEvent event) {
-        // Moddaki TÜM yaratıkların can, hız ve saldırı attribute'larını eksiksiz kaydediyoruz
-        event.put(ModEntities.HUMANOID.get(), Humanoid.createAttributes().build());
-        event.put(ModEntities.CREATURE1.get(), Creature1.createAttributes().build());
-        event.put(ModEntities.CREATURE2.get(), Creature2.createAttributes().build());
-        event.put(ModEntities.CREATURE3.get(), Creature3.createAttributes().build());
+        if (ModEntities.HUMANOID.isPresent()) {
+            event.put(ModEntities.HUMANOID.get(), Humanoid.createAttributes().build());
+        }
+        if (ModEntities.CREATURE1.isPresent()) {
+            event.put(ModEntities.CREATURE1.get(), Creature1.createAttributes().build());
+        }
+        if (ModEntities.CREATURE2.isPresent()) {
+            event.put(ModEntities.CREATURE2.get(), Creature2.createAttributes().build());
+        }
+        if (ModEntities.CREATURE3.isPresent()) {
+            event.put(ModEntities.CREATURE3.get(), Creature3.createAttributes().build());
+        }
     }
 }
