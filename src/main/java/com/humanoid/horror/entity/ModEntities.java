43450f0fbf8febb8fc533605fae5d293054fc048
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-02 07:42:48 +0300

    Update ModEntities.java

--- src/main/java/com/humanoid/horror/entity/ModEntities.java
+++ src/main/java/com/humanoid/horror/entity/ModEntities.java
@@ -1,1 +1,19 @@
+package com.humanoid.horror.entity;
 
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModEntities {
+
+    public static final RegistryObject<EntityType<Humanoid>> HUMANOID =
+            com.humanoid.horror.registry.ModEntities.HUMANOID;
+
+    public static final RegistryObject<EntityType<Creature1>> CREATURE1 =
+            com.humanoid.horror.registry.ModEntities.CREATURE1;
+
+    public static final RegistryObject<EntityType<Creature2>> CREATURE2 =
+            com.humanoid.horror.registry.ModEntities.CREATURE2;
+
+    public static final RegistryObject<EntityType<Creature3>> CREATURE3 =
+            com.humanoid.horror.registry.ModEntities.CREATURE3;
+}
