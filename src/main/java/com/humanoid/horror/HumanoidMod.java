242f66b48dee378e9097ccf6eec03e4005030b26
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-02 06:43:29 +0300

    Update HumanoidMod.java

--- src/main/java/com/humanoid/horror/HumanoidMod.java
+++ src/main/java/com/humanoid/horror/HumanoidMod.java
@@ -1,26 +1,18 @@
 package com.humanoid.horror;
 
 import com.humanoid.horror.entity.Creature3;
-import com.humanoid.horror.entity.Humanoid;
 import com.humanoid.horror.network.ModMessages;
 import com.humanoid.horror.registry.ModBlocks;
 import com.humanoid.horror.registry.ModEntities;
 import com.humanoid.horror.registry.ModSounds;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Monster;
-
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod("humanoid")
 @Mod.EventBusSubscriber(
@@ -36,47 +28,6 @@ public class HumanoidMod {
     public static int currentDay = 1;
 
     // =========================================================
-    // ENTITY REGISTRY
-    // =========================================================
-
-    public static final DeferredRegister<EntityType<?>> ENTITIES =
-            DeferredRegister.create(
-                    ForgeRegistries.ENTITY_TYPES,
-                    MOD_ID
-            );
-
-    // =========================================================
-    // HUMANOID
-    // =========================================================
-
-    public static final RegistryObject<EntityType<Humanoid>> HUMANOID =
-            ENTITIES.register(
-                    "humanoid",
-                    () -> EntityType.Builder.of(
-                            Humanoid::new,
-                            MobCategory.MONSTER
-                    )
-                    .sized(0.6F, 1.95F)
-                    .build("humanoid")
-            );
-
-    // =========================================================
-    // CREATURE3
-    // =========================================================
-
-    public static final RegistryObject<EntityType<Creature3>> CREATURE3 =
-            ENTITIES.register(
-                    "creature3",
-                    () -> EntityType.Builder.of(
-                            Creature3::new,
-                            MobCategory.MONSTER
-                    )
-                    .sized(0.8F, 2.2F)
-                    .clientTrackingRange(8)
-                    .build("creature3")
-            );
-
-    // =========================================================
     // CONSTRUCTOR
     // =========================================================
 
@@ -87,19 +38,17 @@ public class HumanoidMod {
                         .get()
                         .getModEventBus();
 
-        // Kendi Entity kayıtların
-        ENTITIES.register(modEventBus);
-
-        // ModEntities içindeki kayıtlar
+        // ENTITY REGISTRY
+        // Bütün entity kayıtları artık sadece ModEntities'den geliyor.
         ModEntities.ENTITIES.register(modEventBus);
 
-        // TPBlock1 / TPBlock2
+        // BLOCK REGISTRY
         ModBlocks.BLOCKS.register(modEventBus);
 
-        // Dimension2 sesleri
+        // SOUND REGISTRY
         ModSounds.SOUNDS.register(modEventBus);
 
-        // Common setup
+        // COMMON SETUP
         modEventBus.addListener(this::setup);
     }
 
@@ -113,7 +62,7 @@ public class HumanoidMod {
 
         event.enqueueWork(() -> {
 
-            // Mevcut network sistemin
+            // Network sistemi
             ModMessages.register();
 
         });
@@ -130,22 +79,23 @@ public class HumanoidMod {
 
         // Humanoid
         event.put(
-                HUMANOID.get(),
-                Monster.createMonsterAttributes()
+                ModEntities.HUMANOID.get(),
+                net.minecraft.world.entity.monster.Monster
+                        .createMonsterAttributes()
                         .add(
-                                Attributes.MAX_HEALTH,
+                                net.minecraft.world.entity.ai.attributes.Attributes.MAX_HEALTH,
                                 40.0D
                         )
                         .add(
-                                Attributes.MOVEMENT_SPEED,
+                                net.minecraft.world.entity.ai.attributes.Attributes.MOVEMENT_SPEED,
                                 0.3D
                         )
                         .add(
-                                Attributes.ATTACK_DAMAGE,
+                                net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE,
                                 6.0D
                         )
                         .add(
-                                Attributes.FOLLOW_RANGE,
+                                net.minecraft.world.entity.ai.attributes.Attributes.FOLLOW_RANGE,
                                 32.0D
                         )
                         .build()
@@ -153,7 +103,7 @@ public class HumanoidMod {
 
         // Creature3
         event.put(
-                CREATURE3.get(),
+                ModEntities.CREATURE3.get(),
                 Creature3.createAttributes().build()
         );
     }
