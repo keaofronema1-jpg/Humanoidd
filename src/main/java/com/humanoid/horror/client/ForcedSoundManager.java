e2afa0e4a36efaabd04b34031468e68704e6952d
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-06 22:53:29 +0300

    Update ForcedSoundManager.java

--- src/main/java/com/humanoid/horror/client/ForcedSoundManager.java
+++ src/main/java/com/humanoid/horror/client/ForcedSoundManager.java
@@ -5,7 +5,7 @@ import net.minecraft.client.Options;
 import net.minecraft.sounds.SoundSource;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientTickEvent;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
