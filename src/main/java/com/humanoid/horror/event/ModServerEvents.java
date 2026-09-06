b229467db805b4a034cbdf66cce8d1ef70977ad7
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-06 19:13:29 +0300

    Update ModServerEvents.java

--- src/main/java/com/humanoid/horror/event/ModServerEvents.java
+++ src/main/java/com/humanoid/horror/event/ModServerEvents.java
@@ -2,25 +2,30 @@ package com.humanoid.horror.event;
 
 import com.humanoid.horror.HumanoidMod;
 import com.humanoid.horror.check.Check;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = HumanoidMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(
+        modid = HumanoidMod.MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.FORGE
+)
 public class ModServerEvents {
 
-    // Sunucu tamamen açıldığında WorldBorder ayarını yapar
+    /*
+     * Sunucu ilk açıldığında başlangıç prison sistemi
+     * yalnızca burada kurulacak.
+     *
+     * Oyuncu logout/login yaptığında tekrar kurulmayacak.
+     */
     @SubscribeEvent
-    public static void onServerStarted(ServerStartedEvent event) {
-        Check.setupInitialPrison(event.getServer());
-    }
+    public static void onServerStarted(
+            ServerStartedEvent event
+    ) {
 
-    // Oyuncu sunucuya katıldığında doğru konumda başlatılmasını garanti eder
-    @SubscribeEvent
-    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-        if (!HumanoidMod.isStartTriggered && event.getEntity().getServer() != null) {
-            Check.setupInitialPrison(event.getEntity().getServer());
-        }
+        Check.setupInitialPrison(
+                event.getServer()
+        );
     }
 }
