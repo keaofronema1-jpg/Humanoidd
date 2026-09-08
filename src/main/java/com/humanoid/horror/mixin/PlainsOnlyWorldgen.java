717a67c646bf806e9687dbea18eab5659d19ed1c
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-08 22:55:44 +0300

    Update PlainsOnlyWorldgen.java

--- src/main/java/com/humanoid/horror/mixin/PlainsOnlyWorldgen.java
+++ src/main/java/com/humanoid/horror/mixin/PlainsOnlyWorldgen.java
@@ -1,9 +1,6 @@
 package com.humanoid.horror.mixin;
 
 import net.minecraft.core.Holder;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.FixedBiomeSource;
@@ -12,16 +9,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import java.util.Optional;
+
 @Mixin(
 targets = "net.minecraft.world.level.levelgen.presets.WorldPresets$Registrar"
 )
 public abstract class PlainsOnlyWorldgen {
-
-private static final ResourceKey<Biome> PLAINS =
-        ResourceKey.create(
-                BuiltInRegistries.BIOME.key(),
-                new ResourceLocation("minecraft", "plains")
-        );
 
 @ModifyArg(
         method = "createOverworldOptions(Lnet/minecraft/world/level/biome/BiomeSource;Lnet/minecraft/core/Holder;)Lnet/minecraft/world/level/dimension/DimensionOptions;",
@@ -35,16 +28,25 @@ private BiomeSource humanoid$forcePlains(
         BiomeSource originalSource
 ) {
 
-    Holder<Biome> plainsHolder =
-            BuiltInRegistries.BIOME
-                    .getHolder(PLAINS)
-                    .orElse(null);
+    Optional<Holder<Biome>> plains =
+            originalSource.possibleBiomes()
+                    .stream()
+                    .filter(holder ->
+                            holder.unwrapKey()
+                                    .map(key ->
+                                            key.location()
+                                                    .toString()
+                                                    .equals("minecraft:plains")
+                                    )
+                                    .orElse(false)
+                    )
+                    .findFirst();
 
-    if (plainsHolder == null) {
-        return originalSource;
+    if (plains.isPresent()) {
+        return new FixedBiomeSource(plains.get());
     }
 
-    return new FixedBiomeSource(plainsHolder);
+    return originalSource;
 }
 
 }
