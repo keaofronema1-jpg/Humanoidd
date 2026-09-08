a18b0b53dc1fb74710eccf00c46c031f12361ccc
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-08 15:36:32 +0300

    Update Creature2Model.java

--- src/main/java/com/humanoid/horror/client/Creature2Model.java
+++ src/main/java/com/humanoid/horror/client/Creature2Model.java
@@ -6,7 +6,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.PartPose;
+import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
