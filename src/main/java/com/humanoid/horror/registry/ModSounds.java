ceeb63fa4ba7e8f58bbb508ed10044ab0c8114b2
keaofronema1-jpg <keaofronema1@gmail.com>
2026-09-08 22:43:30 +0300

    Update ModSounds.java

--- src/main/java/com/humanoid/horror/registry/ModSounds.java
+++ src/main/java/com/humanoid/horror/registry/ModSounds.java
@@ -11,129 +11,130 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModSounds {
 
-    public static final DeferredRegister<SoundEvent> SOUNDS =
-            DeferredRegister.create(
-                    Registries.SOUND_EVENT,
-                    HumanoidMod.MOD_ID
-            );
+public static final DeferredRegister<SoundEvent> SOUNDS =
+        DeferredRegister.create(
+                Registries.SOUND_EVENT,
+                HumanoidMod.MOD_ID
+        );
 
-    // =========================================================
-    // scare.ogg
-    // =========================================================
+// =========================================================
+// scare.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> SCARE =
-            SOUNDS.register(
-                    "scare",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "scare"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> SCARE =
+        SOUNDS.register(
+                "scare",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "scare"
+                        )
+                )
+        );
 
-    // =========================================================
-    // wep.ogg
-    // =========================================================
+// =========================================================
+// wep.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> WEP =
-            SOUNDS.register(
-                    "wep",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "wep"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> WEP =
+        SOUNDS.register(
+                "wep",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "wep"
+                        )
+                )
+        );
 
-    // =========================================================
-    // cont.ogg
-    // =========================================================
+// =========================================================
+// cont.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> CONT =
-            SOUNDS.register(
-                    "cont",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "cont"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> CONT =
+        SOUNDS.register(
+                "cont",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "cont"
+                        )
+                )
+        );
 
-    // =========================================================
-    // dimension2music.ogg
-    // =========================================================
+// =========================================================
+// dimension2music.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> DIMENSION2_MUSIC =
-            SOUNDS.register(
-                    "dimension2music",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "dimension2music"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> DIMENSION2_MUSIC =
+        SOUNDS.register(
+                "dimension2music",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "dimension2music"
+                        )
+                )
+        );
 
-    // =========================================================
-    // dimension2music2.ogg
-    // =========================================================
+// =========================================================
+// dimension2music2.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> DIMENSION2_MUSIC2 =
-            SOUNDS.register(
-                    "dimension2music2",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "dimension2music2"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> DIMENSION2_MUSIC2 =
+        SOUNDS.register(
+                "dimension2music2",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "dimension2music2"
+                        )
+                )
+        );
 
-    // =========================================================
-    // entity.ogg
-    // =========================================================
+// =========================================================
+// entity.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> PHOTO_ENTITY =
-            SOUNDS.register(
-                    "entity",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "entity"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> PHOTO_ENTITY =
+        SOUNDS.register(
+                "entity",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "entity"
+                        )
+                )
+        );
 
-    // =========================================================
-    // random.ogg
-    // =========================================================
+// =========================================================
+// random.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> RANDOM =
-            SOUNDS.register(
-                    "random",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "random"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> RANDOM =
+        SOUNDS.register(
+                "random",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "random"
+                        )
+                )
+        );
 
-    // =========================================================
-    // forge.ogg
-    // =========================================================
+// =========================================================
+// forge.ogg
+// =========================================================
 
-    public static final RegistryObject<SoundEvent> FORGE_INTRO =
-            SOUNDS.register(
-                    "forge_intro",
-                    () -> SoundEvent.createVariableRangeEvent(
-                            new ResourceLocation(
-                                    HumanoidMod.MOD_ID,
-                                    "forge_intro"
-                            )
-                    )
-            );
+public static final RegistryObject<SoundEvent> FORGE =
+        SOUNDS.register(
+                "forge",
+                () -> SoundEvent.createVariableRangeEvent(
+                        new ResourceLocation(
+                                HumanoidMod.MOD_ID,
+                                "forge"
+                        )
+                )
+        );
+
 }
